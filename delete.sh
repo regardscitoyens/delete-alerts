bf99cdc49306ce5bb5100e7ab1550cc7986bb52f
Brice Person <brice@ideeslibres.org>
2015-10-03 00:46:45 +0200

    Delete survive to 302

--- delete.sh
+++ delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl --data "confirmed=Oui,%20je%20veux%20la%20supprimer&verif=$2" $1$2 > /dev/null 2>&1
+curl -L --post302 --data "confirmed=Oui,%20je%20veux%20la%20supprimer&verif=$2" $1$2 > /dev/null 2>&1
 echo "$1$2 deleted by $3"
