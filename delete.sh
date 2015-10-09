148de3ffa232fb1485ff53c4d5c41d7148e40331
Brice Person <brice@ideeslibres.org>
2015-10-09 10:06:23 +0200

    oups

--- delete.sh
+++ delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#curl -L --post302 --data "confirmed=Oui,%20je%20veux%20la%20supprimer&verif=$3" $2$3 > /dev/null 2>&1
+curl -L --post302 --data "confirmed=Oui,%20je%20veux%20la%20supprimer&verif=$3" $2$3 > /dev/null 2>&1
 echo "$1 $2$3 de $4 supprimée par $5"
